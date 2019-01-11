57266f0aa56c2e0e3d641d8f6fd93da9cf1cd905
POTremblay95 <46535797+POTremblay95@users.noreply.github.com>
2019-01-11 15:44:11 -0500

    Voici mon code du devoir 0

---
+++ Devoir0_PierreOlivierTremblay.js
@@ -0,0 +1,21 @@
+//Pierre-Olivier Tremblay - 20049076
+
+//Partie 1: Salutations
+
+var prenom = "Pierre-Olivier";
+var nom = "Tremblay";
+
+var salutation = "Bonjour, nom nom est "+prenom+ " "+nom + "!";
+
+//Partie 2: Informations
+
+var age = 23;
+var discipline = "informatique";
+
+var informations = "J'ai "+age+" ans et j'étudie en "+discipline+"!";
+
+//Partie 3: Affichage introduction
+
+var introduction = salutation +"\n"+informations;
+
+print(introduction);
